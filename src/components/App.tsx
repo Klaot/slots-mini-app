6d2549b12867d31d91d49f7a2615d6e3cdef8140
chuikoffru <chuikoff.ru@gmail.com>
2024-04-24 00:16:45 +0300

    added tgui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,31 +2,39 @@ import {
   createNavigator,
   useBackButtonIntegration,
   useNavigatorIntegration,
-} from '@tma.js/react-router-integration';
-import { useBackButton } from '@tma.js/sdk-react';
-import { type FC, useMemo } from 'react';
-import {
-  Navigate,
-  Route,
-  Router,
-  Routes,
-} from 'react-router-dom';
+} from "@tma.js/react-router-integration";
+import { useBackButton, useInitData } from "@tma.js/sdk-react";
+import { useMemo, type FC } from "react";
+import { Navigate, Route, Router, Routes } from "react-router-dom";
+import { AppRoot, Cell, Avatar } from "@xelene/tgui";
 
-import { routes } from '~/navigation/routes.tsx';
+import { routes } from "~/navigation/routes.tsx";
+import "@xelene/tgui/dist/styles.css";
 
 export const App: FC = () => {
   const tmaNavigator = useMemo(createNavigator, []);
   const [location, navigator] = useNavigatorIntegration(tmaNavigator);
   const backButton = useBackButton();
+  const initData = useInitData();
 
   useBackButtonIntegration(tmaNavigator, backButton);
 
   return (
-    <Router location={location} navigator={navigator}>
-      <Routes>
-        {routes.map((route) => <Route key={route.path} {...route} />)}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <AppRoot>
+      <Cell
+        before={<Avatar size={48} src={initData?.user?.photoUrl} />}
+        description={`@${initData?.user?.username}`}
+      >
+        {initData?.user?.firstName} {initData?.user?.lastName}
+      </Cell>
+      <Router location={location} navigator={navigator}>
+        <Routes>
+          {routes.map((route) => (
+            <Route key={route.path} {...route} />
+          ))}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </AppRoot>
   );
 };
--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,32 +1,11 @@
-import type { FC } from 'react';
+import type { FC } from "react";
 
-import { Link } from '~/components/Link/Link.tsx';
-import { Page } from '~/components/Page/Page.tsx';
-import { routes } from '~/navigation/routes.tsx';
+import { Link } from "~/components/Link/Link.tsx";
+import { Page } from "~/components/Page/Page.tsx";
+import { routes } from "~/navigation/routes.tsx";
 
-import './IndexPage.css';
+import "./IndexPage.css";
 
 export const IndexPage: FC = () => {
-  return (
-    <Page title="Home Page">
-      <p>
-        This page is a home page in this boilerplate. You can use the links below to visit other
-        pages with their own functionality.
-      </p>
-      <ul className="index-page__links">
-        {routes.map(({ path, title, icon }) => title && (
-          <li className="index-page__link-item" key={path}>
-            <Link className="index-page__link" to={path}>
-              {icon && (
-                <i className="index-page__link-icon">
-                  {icon}
-                </i>
-              )}
-              {title}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </Page>
-  );
+  return <Page title="Типы событий"></Page>;
 };
